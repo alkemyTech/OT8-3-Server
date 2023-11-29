dc8466b886876883b229520d73345a205aa5ec00
Sun <santiago.lizondo@gmail.com>
2023-11-29 13:31:35 -0300

    db funcional

--- src/main/java/com/alkemy/wallet/WalletApplication.java
+++ src/main/java/com/alkemy/wallet/WalletApplication.java
@@ -2,10 +2,12 @@ package com.alkemy.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.logging.Logger;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WalletApplication {
 
 	public static void main(String[] args) {

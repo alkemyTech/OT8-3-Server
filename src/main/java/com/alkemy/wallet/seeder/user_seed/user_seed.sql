6cc599beb27e9bcacbfe946cadac188868fd0f4f
Sun <santiago.lizondo@gmail.com>
2023-12-04 17:38:44 -0300

    primer seed (#5)

---
+++ src/main/java/com/alkemy/wallet/seeder/user_seed/user_seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO `user` (`email`, `first_name`, `last_name`, `password`)
+VALUES
+    ('juan1@gmail.com', 'Juan', 'Perez', '123'),
+    ('simon2@gmail.com', 'Simon', 'Cacho', '123'),
+    ('pipo3@gmail.com', 'Roberto', 'Lasio', '123'),
+    ('carolina4@gmail.com', 'Caro', 'Piza', '123'),
+    ('cachitox5@gmail.com', 'Chiche', 'Casa', '123'),
+    ('mila6@gmail.com', 'Milas', 'Rosa', '123'),
+    ('user7@gmail.com', 'User', 'Seven', '123'),
+    ('user8@gmail.com', 'User', 'Eight', '123'),
+    ('user9@gmail.com', 'User', 'Nine', '123'),
+    ('user10@gmail.com', 'User', 'Ten', '123'),
+    ('user11@gmail.com', 'User', 'Eleven', '123'),
+    ('user12@gmail.com', 'User', 'Twelve', '123'),
+    ('user13@gmail.com', 'User', 'Thirteen', '123'),
+    ('user14@gmail.com', 'User', 'Fourteen', '123'),
+    ('user15@gmail.com', 'User', 'Fifteen', '123'),
+    ('user16@gmail.com', 'User', 'Sixteen', '123'),
+    ('user17@gmail.com', 'User', 'Seventeen', '123'),
+    ('user18@gmail.com', 'User', 'Eighteen', '123'),
+    ('user19@gmail.com', 'User', 'Nineteen', '123'),
+    ('user20@gmail.com', 'User', 'Twenty', '123');

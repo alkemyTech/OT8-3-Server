c8f6bc23c6bfcd5e69dd4d67e799b7092fcb286c
Sun <santiago.lizondo@gmail.com>
2023-12-06 17:07:03 -0300

    endpoint listo

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -2,6 +2,7 @@ package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,6 +27,16 @@ public class UserController {
         return userList.isEmpty()
                 ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
                 : new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") Long id) {
+        try {
+            User user = this.userService.getUserById(id);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")

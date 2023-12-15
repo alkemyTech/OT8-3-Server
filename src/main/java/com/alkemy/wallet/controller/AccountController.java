efa1fc373d34ed3b45ef44b10582555efab0bf67
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2023-12-15 20:35:31 -0300

    ticket t8s3-31 (#18)

--- src/main/java/com/alkemy/wallet/controller/AccountController.java
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -1,15 +1,14 @@
 package com.alkemy.wallet.controller;
 
+import com.alkemy.wallet.dto.AccountRequestDTO;
+import com.alkemy.wallet.dto.AccountResponseDTO;
 import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,10 @@ public class AccountController {
     public ResponseEntity<List<Account>> getAccountsByUserId(@PathVariable Long id) {
         List<Account> account = accountService.getAccountsByUserId(id);
         return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+    @PostMapping()
+    public ResponseEntity<AccountResponseDTO> createAccount (@RequestBody AccountRequestDTO accountRequestDTO){
+        return new ResponseEntity<>(accountService.createAccount(accountRequestDTO), HttpStatus.CREATED);
     }
 }
 
--- src/main/java/com/alkemy/wallet/service/AccountService.java
+++ src/main/java/com/alkemy/wallet/service/AccountService.java
@@ -1,22 +1,50 @@
 package com.alkemy.wallet.service;
+import com.alkemy.wallet.dto.AccountRequestDTO;
+import com.alkemy.wallet.dto.AccountResponseDTO;
+import com.alkemy.wallet.enums.CurrencyEnum;
 import com.alkemy.wallet.model.Account;
+import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.repository.AccountRepository;
+import com.alkemy.wallet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class AccountService {
 
-    @Autowired
+
     private AccountRepository accountRepository;
+    private UserRepository userRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository, UserRepository userRepository) {
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
+    }
 
     public List<Account> getAccountsByUserId(Long id){
 
         return accountRepository.getAccountsByUserId(id);
     }
 
+    public AccountResponseDTO createAccount (AccountRequestDTO accountRequestDTO){
+        Double transactionLimit = accountRequestDTO.getCurrency().toUpperCase().equals("ARS") ? 300000.0 : 1000.0;
+        User user =  userRepository.findById(Long.decode(accountRequestDTO.getUserAuthenticated())).orElseThrow(()-> new IllegalStateException("User not found"));
 
-
+        Account account = new Account(
+                user,
+                CurrencyEnum.valueOf(accountRequestDTO.getCurrency().toUpperCase()),
+                transactionLimit
+        );
+        Account savedAccount = accountRepository.save(account);
+        AccountResponseDTO response = new AccountResponseDTO();
+        response.setCurrency(savedAccount.getCurrencyEnum().name());
+        response.setBalance(savedAccount.getBalance());
+        response.setUserId(user.getId().toString());
+        response.setTransactionLimit(savedAccount.getTransactionLimit());
+        return response;
+    }
 }

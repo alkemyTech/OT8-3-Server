e9822c2f9e956be5d041c683f49cc99604f75cd7
EmilianoGonzaleztdf <emilianogonzalez@tdf.edu.ar>
2023-12-15 22:15:57 -0300

    ticket t8s3-32

--- src/main/java/com/alkemy/wallet/controller/TransactionsController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionsController.java
@@ -2,6 +2,7 @@ package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.dto.DepositPaymentDTO;
 import com.alkemy.wallet.dto.DepositPaymentResponseDTO;
+import com.alkemy.wallet.dto.IncomeTransactionDTO;
 import com.alkemy.wallet.dto.TransactionDTO;
 import com.alkemy.wallet.service.TransactionsService;
 import jakarta.validation.Valid;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,5 +42,11 @@ public class TransactionsController {
     @GetMapping("/{id}/")
     public TransactionDTO getTransactionById(@PathVariable Long id) {
         return transactionsService.getTransactionById(id);
+    }
+    @PostMapping("/sendArs")
+    public ResponseEntity<?> sendArs(@RequestBody IncomeTransactionDTO incomeTransactionDTO){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        this.transactionsService.sendArs(incomeTransactionDTO , authentication.getName());
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
